d4f12f1640598ed64e58b8794703a7a3769c96f7
Severin Sprenger <sevisprengger@gmail.com>
2022-03-23 19:23:11 +0100

    Can now get pub IP

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,20 @@
-// import custom functions
 import { getCloudflareToken, getApexToken } from './get-tokens'
+import fetch from "node-fetch";
 
-console.log(getCloudflareToken())
-console.log(getApexToken())
+var response;
+
+async function getPubIP()
+{
+    response = await fetch('https://api.ipify.org?format=json')
+    response = await response.json()
+    return response['ip']
+}
+
+async function main()
+{
+    console.log(getCloudflareToken())
+    console.log(getApexToken())
+    console.log(await getPubIP())
+}
+
+main()
