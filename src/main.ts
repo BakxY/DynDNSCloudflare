fe6008626fc327bd156825cedf5cfcf608d142bd
Severin Sprenger <sevisprengger@gmail.com>
2022-04-05 20:00:25 +0200

    I han gnueg für hüt

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,23 @@
 import { getPubIP } from './get-ip'
 import { getCloudflareReadToken, getCloudflareWriteToken, getCloudflareZoneID } from './get-tokens'
 import { getRecords, editRecord } from './cloudflare-api'
-import { getConf, getInterval, getIPOutput } from './read-conf'	
+import { getConf, getInterval, getIPOutput, getUseRecordList, getRecordList } from './read-conf'	
 
 // define variables for current and old IP's
 let currentIP = ''
 let oldPubIP = ''
 
 // get the config file
-let configString = getConf()
+const configString = getConf()
 
 // get the interval config
 const interval = getInterval(configString)
 
 // get the ip every interval config
 const OutIPInterval = getIPOutput(configString)
+
+// get the use_record_list config
+const UseRecordList = getUseRecordList(configString)
 
 // create a async main function
 async function main()
@@ -25,30 +28,56 @@ async function main()
     // check if the public ip has changed or is currently undefined
     if(currentIP != oldPubIP && currentIP != undefined)
     {
+        let JsonRecords = await getRecords(getCloudflareZoneID(), getCloudflareReadToken())
+
         // print a info to the console
         console.info('[ INFO ] Public IP has changed to ' + currentIP)
 
-        let JsonRecords = await getRecords(getCloudflareZoneID(), getCloudflareReadToken())
-
-        // loop through all the records
-        for(let index in JsonRecords)
+        if(UseRecordList == 'true')
         {
-            // check if a record contains the old ip
-            if(JsonRecords[index]['content'] == oldPubIP)
+            // get the record list
+            const recordList = getRecordList()
+
+            // loop through the record list
+            for(let index in JsonRecords)
             {
-                // create a json body to send to the api
-                let APIJsonBody = JSON.stringify({ 
-                    'type': JsonRecords[index]['type'], 
-                    'name': JsonRecords[index]['name'], 
-                    'content': currentIP.toString(), 
-                    'ttl': JsonRecords[index]['ttl'],
-                    'proxied' : JsonRecords[index]['proxied']
-                })
+                if(JsonRecords[index]['type'] == 'A')
+                {
+                    console.log('- ' + JsonRecords[index]['name'])
 
-                await editRecord(getCloudflareZoneID(), getCloudflareWriteToken(), JsonRecords[index]['id'], APIJsonBody)
+                    for(let recordName in recordList)
+                    {
+                        if(JsonRecords[index]['name'] == recordList[recordName])
+                        {
+                            console.log(' + ' + recordList[recordName])
+                        }
+                    }
+                    console.log('\n')
+                }
+            }
+        }
+        else
+        {
+            // loop through all the records
+            for(let index in JsonRecords)
+            {
+                // check if a record contains the old ip
+                if(JsonRecords[index]['content'] == oldPubIP)
+                {
+                    // create a json body to send to the api
+                    let APIJsonBody = JSON.stringify({ 
+                        'type': JsonRecords[index]['type'], 
+                        'name': JsonRecords[index]['name'], 
+                        'content': currentIP.toString(), 
+                        'ttl': JsonRecords[index]['ttl'],
+                        'proxied' : JsonRecords[index]['proxied']
+                    })
 
-                // print the name of the record and a message
-                console.info('[ UPDATE ] Changed DNS record ' + JsonRecords[index]['name'])
+                    await editRecord(getCloudflareZoneID(), getCloudflareWriteToken(), JsonRecords[index]['id'], APIJsonBody)
+
+                    // print the name of the record and a message
+                    console.info('[ UPDATE ] Changed DNS record ' + JsonRecords[index]['name'])
+                }
             }
         }
 
--- src/read-conf.ts
+++ src/read-conf.ts
@@ -2,6 +2,7 @@ import fs from 'fs';
 
 // set file locations
 const conf_file = './resources/configs/main.conf'
+const record_list_file = './resources/configs/record_list.conf'
 
 export function getConf() 
 {
@@ -27,6 +28,36 @@ export function getConf()
 
         // return the string
         return confNoComments
+    }
+    else // error handling
+    {
+        // print a error and exit
+        console.error('[ ERROR ] Config file is empty!')
+        process.exit()
+    }
+}
+
+export function getRecordList() 
+{
+    // define a variable for the token
+    let recordList = ''
+
+    // read token file
+    recordList = fs.readFileSync(record_list_file, 'utf-8')
+
+    // check if the token files is empty
+    if(recordList != '')
+    {
+        // split the sting into an array
+        let recordListArray = recordList.split('\n');
+
+        // remove all comments
+        let recordListNoCommentsArray = recordListArray.filter(function (line) {
+            return line.indexOf('#') != 0;
+        });
+        
+        // return the string
+        return recordListNoCommentsArray
     }
     else // error handling
     {
@@ -101,5 +132,34 @@ export function getIPOutput(confString)
 
         // return the ip_output
         return confIPOutput
+    }
+}
+
+export function getUseRecordList(confString)
+{
+    // split the sting into an array
+    let confArray = confString.split('\n');
+
+    // search string for line with content
+    let confUseRecordListArray = confArray.filter(function (line) {
+        if(line.includes('use_record_list'))
+        {
+            return line
+        }
+    });
+
+    if(confUseRecordListArray == '') // error handling
+    {
+        // print a error and exit
+        console.error('[ ERROR ] Config file is missing the use_record_list!')
+        process.exit()
+    }
+    else
+    {
+        // clean up string
+        let confUseRecordList = confUseRecordListArray[0].replace(/ /g, '').replace(/\t/g, '').replace('use_record_list', '')
+
+        // return the use_record_list
+        return confUseRecordList
     }
 }
