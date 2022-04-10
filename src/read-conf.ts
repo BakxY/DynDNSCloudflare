678c105f538208ad0777906ecee21537839f680d
Severin Sprenger <sevisprengger@gmail.com>
2022-04-10 18:44:12 +0200

    Cleaned up code, is now usable with multible domain/zones

--- src/read-conf.ts
+++ src/read-conf.ts
@@ -3,6 +3,7 @@ import fs from 'fs';
 // set file locations
 const conf_file = './resources/configs/main.conf'
 const record_list_file = './resources/configs/record_list.conf'
+const zones_file = './resources/configs/zones.conf'
 
 export function getConf() 
 {
@@ -33,6 +34,157 @@ export function getConf()
     {
         // print a error and exit
         console.error('[ ERROR ] Config file is empty!')
+        process.exit()
+    }
+}
+
+export function getZones() 
+{
+    // define a variable for the token
+    let zonesContent = ''
+
+    // read token file
+    zonesContent = fs.readFileSync(zones_file, 'utf-8')
+
+    // check if the token files is empty
+    if(zonesContent != '')
+    {
+        // split the sting into an array
+        let zonesArray = zonesContent.split('\n');
+
+        // remove all comments
+        let zonesNoCommentsArray = zonesArray.filter(function (line) {
+            return line.replace(/ /g, '').replace(/\t/g, '').replace('{', '').indexOf('#') != 0;
+        });
+
+        // loop through all lines and remove all spaces and tabs
+        for(let index in zonesNoCommentsArray)
+        {
+            zonesNoCommentsArray[index] = zonesNoCommentsArray[index].replace(/ /g, '').replace(/\t/g, '').replace(/\r/g, '').replace(/'/g, '')
+        }
+
+        // declare the zone data array
+        let zoneArray = []
+
+        for(let index in zonesNoCommentsArray)
+        {
+            // check if the line contains a new zone
+            if(zonesNoCommentsArray[index].includes('Zone') && (zonesNoCommentsArray[index].includes('{') || zonesNoCommentsArray[Number(index) + 1].includes('{')))
+            {
+                let IsolatedZoneData = ''
+                for(let IsolateIndex = Number(index); IsolateIndex < zonesNoCommentsArray.length; IsolateIndex++)
+                {
+                    // check if the line contains a new zone
+                    if(zonesNoCommentsArray[IsolateIndex].includes('}'))
+                    {
+                        // set the zone data
+                        IsolatedZoneData += zonesNoCommentsArray[IsolateIndex]
+                        break
+                    }
+                    else
+                    {
+                        // set the zone data
+                        IsolatedZoneData += zonesNoCommentsArray[IsolateIndex] + '\n'
+                    }
+                }
+
+                // error handling
+                if(!IsolatedZoneData.includes('}') || !IsolatedZoneData.includes('{'))
+                {
+                    console.error('[ ERROR ] Zone is not properly defined! Missing {} somewhere!')
+                    process.exit()
+                }
+
+                if(!IsolatedZoneData.includes('alias'))
+                {
+                    console.error('[ ERROR ] No alias defined in zone definition!')
+                    process.exit()
+                }
+
+                if(!IsolatedZoneData.includes('base-domain'))
+                {
+                    console.error('[ ERROR ] No base domain defined in zone definition!')
+                    process.exit()
+                }
+
+                if(!IsolatedZoneData.includes('zone-id'))
+                {
+                    console.error('[ ERROR ] No base zone id defined in zone definition!')
+                    process.exit()
+                }
+
+                if(!IsolatedZoneData.includes('zone-read-token'))
+                {
+                    console.error('[ ERROR ] No read token defined in zone definition!')
+                    process.exit()
+                }
+
+                if(!IsolatedZoneData.includes('zone-write-token'))
+                {
+                    console.error('[ ERROR ] No write token defined in zone definition!')
+                    process.exit()
+                }
+
+                for(let DupeIdex in zoneArray)
+                {
+                    if(IsolatedZoneData.includes(zoneArray[DupeIdex]['ZoneDomain']))
+                    {
+                        console.error('[ ERROR ] Duplicate domains!')
+                        process.exit()
+                    }
+                }
+
+                let IsolatedZoneDataArray = []
+
+                // split the isolated zone data into an array
+                IsolatedZoneDataArray = IsolatedZoneData.split('\n')
+
+                // create a new json object
+                let ZoneData = JSON.parse(
+                    '{"ZoneAlias":"",' +
+                    '"ZoneDomain":"",' +
+                    '"id":"",' +
+                    '"readToken":"",' +
+                    '"writeToken":""}'
+                )
+
+                // populate the zone data
+                for(let PopulateIndex in IsolatedZoneDataArray)
+                {
+                    if(IsolatedZoneDataArray[Number(PopulateIndex)].includes('alias'))
+                    {
+                        ZoneData['ZoneAlias'] = IsolatedZoneDataArray[PopulateIndex].replace('alias', '').replace('=', '').replace('\r', '').replace('\n', '')
+                    }
+
+                    if(IsolatedZoneDataArray[Number(PopulateIndex)].includes('base-domain'))
+                    {
+                        ZoneData['ZoneDomain'] = IsolatedZoneDataArray[PopulateIndex].replace('base-domain', '').replace('=', '').replace('\r', '').replace('\n', '')
+                    }
+
+                    if(IsolatedZoneDataArray[Number(PopulateIndex)].includes('zone-id'))
+                    {
+                        ZoneData['id'] = IsolatedZoneDataArray[PopulateIndex].replace('zone-id', '').replace('=', '').replace('\r', '').replace('\n', '')
+                    }
+
+                    if(IsolatedZoneDataArray[Number(PopulateIndex)].includes('zone-read-token'))
+                    {
+                        ZoneData['readToken'] = IsolatedZoneDataArray[PopulateIndex].replace('zone-read-token', '').replace('=', '').replace('\r', '').replace('\n', '')
+                    }
+
+                    if(IsolatedZoneDataArray[Number(PopulateIndex)].includes('zone-write-token'))
+                    {
+                        ZoneData['writeToken'] = IsolatedZoneDataArray[PopulateIndex].replace('zone-write-token', '').replace('=', '').replace('\r', '').replace('\n', '')
+                    }
+                }
+                zoneArray.push(ZoneData)
+            }
+        }
+        return zoneArray
+    }
+    else // error handling
+    {
+        // print a error and exit
+        console.error('[ ERROR ] Zones file is empty!')
         process.exit()
     }
 }
@@ -137,7 +289,14 @@ export function getIPOutput(confString)
         let confIPOutput = confIPOutputArray[0].replace(/ /g, '').replace(/\t/g, '').replace('ip_output', '').replace('\r', '')
 
         // return the ip_output
-        return confIPOutput
+        if(confIPOutput === 'true')
+        {
+            return true
+        }
+        else
+        {
+            return false
+        }
     }
 }
 
@@ -165,7 +324,14 @@ export function getUseRecordList(confString)
         // clean up string
         let confUseRecordList = confUseRecordListArray[0].replace(/ /g, '').replace(/\t/g, '').replace('use_record_list', '').replace('\r', '')
 
-        // return the use_record_list
-        return confUseRecordList
+        // return the use_record_list config as a boolean
+        if(confUseRecordList === 'true')
+        {
+            return true
+        }
+        else
+        {
+            return false
+        }
     }
 }
