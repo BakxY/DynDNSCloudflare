d1edd26e37720dc6a1d1e918e5e8f5f99a1ecd4e
Severin Sprenger <sevisprengger@gmail.com>
2022-04-19 20:13:39 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -59,7 +59,7 @@ rm -f /etc/systemd/system/DynDNSCloudflare.service &> /dev/null
 echo "Getting source code and unzipping"
 wget https://github.com/BakxY/DynDNSCloudflare/archive/refs/tags/V1-release.zip -P /tmp/ &> /dev/null
 unzip /tmp/V1-release.zip -d /usr/local/ &> /dev/null
-mv /usr/local/DynDNSCloudflare-V1-release /usr/local/DynDNSCloudflare
+mv /usr/local/DynDNSCloudflare-1-release /usr/local/DynDNSCloudflare
 
 echo "Moving service file"
 mv /usr/local/DynDNSCloudflare/DynDNSCloudflare.service /etc/systemd/system/DynDNSCloudflare.service
