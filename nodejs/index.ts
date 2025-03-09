403ec36648ef51686b692a046d83af5fc3bb05ec
Severin Sprenger <severin.sprenger@sespra.ch>
2025-03-09 11:42:29 +0100

    Fixed types in main file

--- nodejs/index.ts
+++ nodejs/index.ts
@@ -3,20 +3,20 @@ import * as cloudflare from './resources/lib/cloudflare-api'
 import * as conf from './resources/lib/read-conf'	
 
 // define variables for current and old IP's
-let currentIP = ''
-let oldPubIP = ''
+let currentIP: string = ''
+let oldPubIP: string = ''
 
 // get the config file
-const configString = conf.getConf()
+const configString: string = conf.getConf()
 
 // get the interval config
-const interval = conf.getInterval(configString)
+const interval: number = conf.getInterval(configString)
 
 // get the ip every interval config
-const OutIPOnInterval = conf.getIPOutput(configString)
+const OutIPOnInterval: boolean = conf.getIPOutput(configString)
 
 // get the use_record_list config
-const UseRecordList = conf.getUseRecordList(configString)
+const UseRecordList: boolean = conf.getUseRecordList(configString)
 
 // create a async main function
 async function main()
@@ -25,7 +25,7 @@ async function main()
     currentIP = await ip.getPubIP(OutIPOnInterval)
 
     // check if the public ip has changed or is currently undefined
-    if(currentIP != oldPubIP && currentIP != undefined)
+    if(currentIP !== oldPubIP && currentIP !== undefined)
     {
         // print a info to the console
         console.info('[ INFO ] Public IP has changed to ' + currentIP)
