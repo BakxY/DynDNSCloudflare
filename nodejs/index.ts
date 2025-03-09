9992b80088c1d121fe80c0d3614322ad68103de6
Severin Sprenger <severin.sprenger@sespra.ch>
2025-03-09 11:43:17 +0100

    Formatted main file

--- nodejs/index.ts
+++ nodejs/index.ts
@@ -1,6 +1,6 @@
 import * as ip from './resources/lib/get-ip'
 import * as cloudflare from './resources/lib/cloudflare-api'
-import * as conf from './resources/lib/read-conf'	
+import * as conf from './resources/lib/read-conf'
 
 // define variables for current and old IP's
 let currentIP: string = ''
@@ -19,14 +19,12 @@ const OutIPOnInterval: boolean = conf.getIPOutput(configString)
 const UseRecordList: boolean = conf.getUseRecordList(configString)
 
 // create a async main function
-async function main()
-{
+async function main() {
     // get the current public ip
     currentIP = await ip.getPubIP(OutIPOnInterval)
 
     // check if the public ip has changed or is currently undefined
-    if(currentIP !== oldPubIP && currentIP !== undefined)
-    {
+    if (currentIP !== oldPubIP && currentIP !== undefined) {
         // print a info to the console
         console.info('[ INFO ] Public IP has changed to ' + currentIP)
 
